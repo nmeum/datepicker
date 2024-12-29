fc5070ecb7ed09632f014186fc1e4ee1c4ba0c24
Sören Tempel <soeren+git@soeren-tempel.net>
2024-12-29 19:51:35 +0100

    Move showView to UI

--- app/Main.hs
+++ app/Main.hs
@@ -1,45 +1,22 @@
 module Main where
 
 import CmdLine (cmdOpts, optDuration, optFormat, optNoTime, optsPeriod)
-import Control.Monad (when)
-import Data.Maybe (fromMaybe, isJust)
 import Data.Time.LocalTime (LocalTime (LocalTime), getZonedTime, localDay, zonedTimeToLocalTime)
 import Graphics.Vty qualified as V
 import Graphics.Vty.Input.Events qualified as E
 import Graphics.Vty.Platform.Unix (mkVtyWithSettings)
 import Graphics.Vty.Platform.Unix.Settings (UnixSettings (settingInputFd, settingOutputFd), defaultSettings)
 import Options.Applicative (execParser)
-import System.Exit (exitFailure)
 import System.Posix.IO (OpenMode (ReadWrite), defaultFileFlags, openFd)
 import UI qualified
 import UI.Month qualified as M
 import UI.Time qualified as T
-import Util (format, horizCenter, vertCenter)
+import Util (format)
 
-isTermEvent :: E.Event -> Bool
-isTermEvent (E.EvKey key _) =
+isTerm :: E.Event -> Bool
+isTerm (E.EvKey key _) =
   key == E.KEsc || key == E.KChar 'q'
-isTermEvent _ = False
-
-showView :: (UI.View a) => a -> V.Vty -> IO LocalTime
-showView v t = showView' v t True
-  where
-    showView' view vty redraw = do
-      let out = V.outputIface vty
-      region <- V.displayBounds out
-
-      when redraw $ do
-        let (w, h) = (V.regionWidth region, V.regionHeight region)
-            img = horizCenter w $ vertCenter h $ UI.draw view
-            pic = V.picForImage img
-        V.update vty pic
-
-      e <- V.nextEvent vty
-      if isTermEvent e
-        then V.shutdown vty >> exitFailure
-        else case UI.process view e of
-          Right output -> pure output
-          Left mv -> showView' (fromMaybe view mv) vty (isJust mv)
+isTerm _ = False
 
 -- Make sure we read and write to /dev/tty instead of relying on stdin/stdout.
 -- This allows using datepicker within pipes where stdin/stdout is redirected.
@@ -60,12 +37,12 @@ main = do
 
   let today = localDay localTime
       range = optsPeriod (optDuration args) today
-  lt@(LocalTime date _) <- showView (M.mkMonthView range today) vty
+  lt@(LocalTime date _) <- UI.showView (M.mkMonthView range today) isTerm vty
 
   if optNoTime args
     then V.shutdown vty >> putStrLn (format outFmt lt)
     else do
       (LocalTime _ nowTime) <- zonedTimeToLocalTime <$> getZonedTime
-      (LocalTime _ time) <- showView (T.mkTimeView nowTime lt) vty
+      (LocalTime _ time) <- UI.showView (T.mkTimeView nowTime lt) isTerm vty
 
       V.shutdown vty >> putStrLn (format outFmt $ LocalTime date time)
--- app/UI.hs
+++ app/UI.hs
@@ -1,9 +1,34 @@
-module UI (View (..)) where
+module UI (View (..), showView) where
 
+import Control.Monad (when)
+import Data.Maybe (fromMaybe, isJust)
 import Data.Time.LocalTime (LocalTime)
+import Graphics.Vty qualified as V
 import Graphics.Vty.Image qualified as I
 import Graphics.Vty.Input.Events qualified as E
+import System.Exit (exitFailure)
+import Util (horizCenter, vertCenter)
 
 class View a where
   draw :: a -> I.Image
   process :: a -> E.Event -> Either (Maybe a) LocalTime
+
+showView :: (View a) => a -> (E.Event -> Bool) -> V.Vty -> IO LocalTime
+showView v isTermEvent t = showView' v t True
+  where
+    showView' view vty redraw = do
+      let out = V.outputIface vty
+      region <- V.displayBounds out
+
+      when redraw $ do
+        let (w, h) = (V.regionWidth region, V.regionHeight region)
+            img = horizCenter w $ vertCenter h $ draw view
+            pic = V.picForImage img
+        V.update vty pic
+
+      e <- V.nextEvent vty
+      if isTermEvent e
+        then V.shutdown vty >> exitFailure
+        else case process view e of
+          Right output -> pure output
+          Left mv -> showView' (fromMaybe view mv) vty (isJust mv)
