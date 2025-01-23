426a9bff528681bd7327d5303654d9d72cf48a17
Sören Tempel <tempel@ibr.cs.tu-bs.de>
2025-01-23 04:24:25 +0100

    test: Move utility functions to own module

--- test/Tmux.hs
+++ test/Tmux.hs
@@ -2,70 +2,9 @@
 
 module Tmux (tmuxTests) where
 
-import Control.Monad (void)
-import Control.Monad.IO.Class (liftIO)
-import Control.Monad.Reader (MonadIO, MonadReader)
-import Control.Monad.State.Class (MonadState)
-import Data.ByteString qualified as B
-import Data.List (find, isSuffixOf)
-import Data.Maybe (fromJust)
-import Data.String (fromString)
 import Test.Tasty
-import Test.Tasty.HUnit (assertBool)
 import Test.Tasty.Tmux
-
--- Timezone used for the tests, set explicitly to ensure that test results
--- are deterministic, independent of the value of the TZ environment variable.
-timezone :: String
-timezone = "CET"
-
--- Header written by datepicker, used to detect application startup.
---
--- Note: This is only a substring of the header so that it works with -m.
-header :: B.ByteString
-header = "Tu We Th Fr Sa"
-
-startApplication ::
-  (HasTmuxSession a, MonadReader a m, MonadIO m) =>
-  [String] ->
-  String ->
-  m ()
-startApplication args dateInput = do
-  let cmd = "datepicker " ++ unwords args ++ " " ++ dateInput
-  _ <-
-    sendLine
-      ("env TZ=" ++ timezone ++ " " ++ cmd)
-      (Substring header)
-  pure ()
-
-captureDate :: (HasTmuxSession a, MonadReader a m, MonadState Capture m, MonadIO m) => m String
-captureDate = do
-  _ <-
-    waitForCondition
-      (Substring $ fromString timezone)
-      defaultRetries
-      defaultBackoff
-
-  out <- captureString <$> (snapshot >> capture)
-  pure $ fromJust $ find (isSuffixOf timezone) (reverse $ lines out)
-
-assertDate :: (MonadIO m) => String -> String -> m ()
-assertDate expected actual = do
-  liftIO $
-    assertBool
-      ( "Date does not match: "
-          ++ expected
-          ++ " <-> "
-          ++ actual
-      )
-      (expected == actual)
-
-sendKeys_ ::
-  (HasTmuxSession a, MonadReader a m, MonadIO m) =>
-  String -> Condition -> m ()
-sendKeys_ s c = void (sendKeys s c)
-
------------------------------------------------------------------------
+import Util
 
 selectDateSpatially :: TestCase sharedEnv
 selectDateSpatially =
@@ -153,7 +92,7 @@ selectTimeAndOverflow =
   withTmuxSession' "select time, delete, and re-enter" $ \_ -> do
     startApplication [] "03 1900"
 
-    sendKeys_ "Enter" (Substring $ fromString "March")
+    sendKeys_ "Enter" (Substring "March")
     sendKeys_ "1111 2222" Unconditional
 
     sendKeys_ "Enter" Unconditional
@@ -164,7 +103,7 @@ selectTimeBackspace =
   withTmuxSession' "select time, delete, and re-enter" $ \_ -> do
     startApplication [] "03 1900"
 
-    sendKeys_ "Enter" (Substring $ fromString "March")
+    sendKeys_ "Enter" (Substring "March")
     sendKeys_ "1 2 3 4" Unconditional
     sendKeys_ "c-h" Unconditional
     sendKeys_ "5" Unconditional
---
+++ test/Util.hs
@@ -0,0 +1,65 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Util where
+
+import Control.Monad (void)
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Reader (MonadIO, MonadReader)
+import Control.Monad.State.Class (MonadState)
+import Data.ByteString qualified as B
+import Data.List (find, isSuffixOf)
+import Data.Maybe (fromJust)
+import Data.String (fromString)
+import Test.Tasty.HUnit (assertBool)
+import Test.Tasty.Tmux
+
+-- Timezone used for the tests, set explicitly to ensure that test results
+-- are deterministic, independent of the value of the TZ environment variable.
+timezone :: String
+timezone = "CET"
+
+-- Header written by datepicker, used to detect application startup.
+--
+-- Note: This is only a substring of the header so that it works with -m.
+header :: B.ByteString
+header = "Tu We Th Fr Sa"
+
+startApplication ::
+  (HasTmuxSession a, MonadReader a m, MonadIO m) =>
+  [String] ->
+  String ->
+  m ()
+startApplication args dateInput = do
+  let cmd = "datepicker " ++ unwords args ++ " " ++ dateInput
+  _ <-
+    sendLine
+      ("env TZ=" ++ timezone ++ " " ++ cmd)
+      (Substring header)
+  pure ()
+
+captureDate :: (HasTmuxSession a, MonadReader a m, MonadState Capture m, MonadIO m) => m String
+captureDate = do
+  _ <-
+    waitForCondition
+      (Substring $ fromString timezone)
+      defaultRetries
+      defaultBackoff
+
+  out <- captureString <$> (snapshot >> capture)
+  pure $ fromJust $ find (isSuffixOf timezone) (reverse $ lines out)
+
+assertDate :: (MonadIO m) => String -> String -> m ()
+assertDate expected actual = do
+  liftIO $
+    assertBool
+      ( "Date does not match: "
+          ++ expected
+          ++ " <-> "
+          ++ actual
+      )
+      (expected == actual)
+
+sendKeys_ ::
+  (HasTmuxSession a, MonadReader a m, MonadIO m) =>
+  String -> Condition -> m ()
+sendKeys_ s c = void (sendKeys s c)
